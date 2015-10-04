c9a00af8ac55199108a148f50a11d7c57744c9b4
Gustavo <gomezf09@gmail.com>
2015-10-03 19:36:34 -0500

    - Se crea la pantalla de login con las validaciones de usuario y clave

--- app/src/main/java/com/instituto/cuanto/sisgene/MainActivity.java
+++ app/src/main/java/com/instituto/cuanto/sisgene/MainActivity.java
@@ -4,14 +4,69 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAceptar, btnSalir;
+    EditText etNombreUsuario, etClave;
+    TextView tvNombreUsuarioError, tvClaveError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAceptar = (Button)findViewById(R.id.btnAceptar);
+        btnSalir = (Button)findViewById(R.id.btnSalir);
+        etNombreUsuario = (EditText)findViewById(R.id.etNombreUsuario);
+        etClave = (EditText)findViewById(R.id.etClave);
+        tvNombreUsuarioError = (TextView)findViewById(R.id.tvNombreUsuarioError);
+        tvClaveError = (TextView)findViewById(R.id.tvClaveError);
+
+
+        btnAceptar.setOnClickListener(btnAceptarsetOnClickListener);
+        btnSalir.setOnClickListener(btnSalirsetOnClickListener);
     }
+
+    View.OnClickListener btnAceptarsetOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            boolean camposOK = true;
+
+            tvNombreUsuarioError.setText("");
+            tvClaveError.setText("");
+
+            if(etNombreUsuario.getText().toString().trim().length() == 0)
+            {
+                tvNombreUsuarioError.setText(getResources().getString(R.string.login_user_error));
+                camposOK = false;
+            }
+            if(etClave.getText().toString().trim().length() == 0)
+            {
+                tvClaveError.setText(getResources().getString(R.string.login_pass_error));
+                camposOK = false;
+            }
+
+            //validar usuario y clave con WebService
+            if(camposOK)
+            {
+
+            }
+
+        }
+    };
+
+    View.OnClickListener btnSalirsetOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
