4b75bd921555e9bd4a28d8b2fcf6c6d2188349da
Jesus <jhonna516@gmail.com>
2015-10-18 20:43:14 -0500

    Rejected commit: Default

---
+++ app/src/main/java/com/instituto/cuanto/sisgene/constantes/Constants.java
@@ -0,0 +1,12 @@
+package com.instituto.cuanto.sisgene.constantes;
+
+public class Constants {
+
+	public static final String FIRST_COLUMN="First";
+	public static final String SECOND_COLUMN="Second";
+	public static final String THIRD_COLUMN="Third";
+	public static final String FOURTH_COLUMN="Fourth";
+	public static final String FIVE_COLUMN="Five";
+	public static final String SIX_COLUMN="Six";
+	public static final String SEVEN_COLUMN="Seven";
+}
