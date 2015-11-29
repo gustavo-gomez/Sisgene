6be4c36d02143c0cd6d4328d544368380e8135f4
Gustavo <gomezf09@gmail.com>
2015-11-29 12:21:01 -0500

    - se esta implementando el retomar encuesta

--- app/src/main/java/com/instituto/cuanto/sisgene/dao/DetalleEncRptaDAO.java
+++ app/src/main/java/com/instituto/cuanto/sisgene/dao/DetalleEncRptaDAO.java
@@ -3,6 +3,8 @@ package com.instituto.cuanto.sisgene.dao;
 import android.content.Context;
 import android.database.Cursor;
 
+import java.util.ArrayList;
+
 /**
  * Created by Jesus on 28/11/2015.
  */
@@ -10,23 +12,26 @@ public class DetalleEncRptaDAO {
 
     public DetalleEncRptaDAO(){}
 
-    public String obtenerRpta(Context context) {
+    public ArrayList<String> obtenerRpta(Context context) {
         Cursor cursor = null;
         DataBaseHelper dataBaseHelper = new DataBaseHelper(context);
 
-        String valorRpta = "";
+        ArrayList<String> valorRpta = null;
 
 
         try{
-            String sql = " select der.deer_valor_respuesta " +
+            String sql = " select der.deer_id, der.deer_valor_respuesta, der.pre_id" +
                     " from pregunta pre " +
                     " inner join det_enc_rpta der on pre.pre_id = der.pre_id ";
 
             cursor = dataBaseHelper.db.rawQuery(sql, null);
 
             if (cursor.moveToFirst()){
-                valorRpta = cursor.getString(0);
+                valorRpta.add(cursor.getString(0)); // id de la respuesta
+                valorRpta.add(cursor.getString(1)); // valor de la respuesta
+                valorRpta.add(cursor.getString(2)); // id de la pregunta asociada a la rpta
             }
+
         }catch(Exception ex){
             ex.printStackTrace();
             return null;
@@ -47,7 +52,7 @@ public class DetalleEncRptaDAO {
 
 
         try{
-            String sql = " select der.deer_valor_respuesta " +
+            String sql = " select der.deer_valor_respuesta, der.pre_id " +
                     " from pregunta pre " +
                     " inner join det_enc_rpta der on pre.pre_id = der.pre_id "+
                     " where pre.pre_id = ?";
